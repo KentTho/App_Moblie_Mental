e9ea41e5cc1f5deefbedf7926552e13550488987
KentTho <havantho2004@gmail.com>
2025-06-27 12:39:00 +0700

    continous Design Emotion

--- mental_health_app/lib/features/diary/page/emotion_entry.dart
+++ mental_health_app/lib/features/diary/page/emotion_entry.dart
@@ -80,39 +80,69 @@ class EmotionEntry extends StatefulWidget{
                       ),
                   ),
                 ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: FaIcon(FontAwesomeIcons.arrowDown),
-                      padding: EdgeInsets.zero,
-                      visualDensity: VisualDensity.compact,
-                      constraints: BoxConstraints(),
-                      style: IconButton.styleFrom(tapTargetSize: MaterialTapTargetSize.shrinkWrap,),
-                      iconSize: 18,
-                      color: gray700,
-                    ),
-                    DropdownButton(
-                      value: dropdownValue,
-                      items: dropdownOptions
-                          .map((e) =>
-                          DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          ))
-                          .toList(),
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropdownValue = newValue!;
-                        });
-                      },
-                    ),
-                    Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: FaIcon(FontAwesomeIcons.bars),
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: FaIcon(FontAwesomeIcons.arrowDown),
+                        padding: EdgeInsets.zero,
+                        visualDensity: VisualDensity.compact,
+                        constraints: BoxConstraints(),
+                        style: IconButton.styleFrom(tapTargetSize: MaterialTapTargetSize.shrinkWrap,),
+                        iconSize: 18,
+                        color: gray700,
+                      ),
+                      SizedBox(width: 16),
+                      DropdownButton(
+                        value: dropdownValue,
+                        icon: Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: FaIcon(
+                            FontAwesomeIcons.arrowDownWideShort,
+                            size: 18,
+                            color: gray700,
+                          ),
+                        ),
+                        underline: SizedBox.shrink(),
+                        borderRadius: BorderRadius.circular(16),
+                        isDense: true,
+                        items: dropdownOptions
+                            .map((e) =>
+                            DropdownMenuItem(
+                              value: e,
+                              child: Row(
+                                children: [
+                                  Text(e),
+                                  if( e == dropdownValue) ...[
+                                    SizedBox(width: 8),
+                                    Icon(Icons.check, color: primaryColor)
+                                  ],
+                                ],
+                              ),
+                            ))
+                            .toList(),
+                        selectedItemBuilder: (context) => dropdownOptions.map((e) => Text(e)).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        },
+                      ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: FaIcon(FontAwesomeIcons.bars),
+                        padding: EdgeInsets.zero,
+                        visualDensity: VisualDensity.compact,
+                        constraints: BoxConstraints(),
+                        style: IconButton.styleFrom(tapTargetSize: MaterialTapTargetSize.shrinkWrap,),
+                        iconSize: 18,
+                        color: gray700,
+                      ),
+                    ],
+                  ),
                 ),
                 Expanded(
                   child: GridView.builder(
@@ -124,33 +154,79 @@ class EmotionEntry extends StatefulWidget{
                     ),
                     itemBuilder: (context, index) {
                       return Container(
-                        padding: EdgeInsets.all(8),
                         decoration: BoxDecoration(
                           color: Colors.white,
+                          border: Border.all(color: primaryColor, width: 2),
                           borderRadius: BorderRadius.circular(12),
                           boxShadow: [
-                            BoxShadow(blurRadius: 4, color: Colors.black12),
+                            BoxShadow(
+                                blurRadius: 4,
+                                color: Colors.black12.withOpacity(0.5),
+                              offset: Offset(4, 4)
+                            ),
                           ],
                         ),
+                        padding: EdgeInsets.all(12),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('This is going to be a title',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            SizedBox(height: 4),
-                            Row(
-                              children: [
-                                Chip(label: Text('First chip')),
-                              ],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: gray900,
+                                ),
                             ),
                             SizedBox(height: 4),
-                            Text('Some content'),
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: List.generate(3, (index) =>
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(16),
+                                          color: gray100),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 12,
+                                          vertical: 4),
+                                      margin: EdgeInsets.only(right: 8),
+                                      child: Text('First chip',
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: gray700
+                                        ),
+                                      ),
+                                    ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Expanded(
+                              child: Text('Some content',
+                                style: TextStyle(
+                                    color: gray700
+                                ),
+                              ),
+                            ),
                             Spacer(),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text('02 November'),
-                                FaIcon(FontAwesomeIcons.trash, size: 16),
+                                Text('02 November',
+                                  style: TextStyle(
+                                      color: gray500,
+                                      fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Spacer(),
+                                FaIcon(
+                                    FontAwesomeIcons.trash,
+                                    size: 16,
+                                    color: gray500,
+                                ),
                               ],
                             ),
                           ],
