a0c31919213397696e218da1b69504cf9a3a38f4
KentTho <havantho2004@gmail.com>
2025-07-20 23:50:17 +0700

    Modified BE: FOreign key notes

--- backend_api/src/models/notes.py
+++ backend_api/src/models/notes.py
@@ -1,14 +1,14 @@
 from sqlalchemy.dialects.postgresql import JSONB  # ✅ Import thêm
-from sqlalchemy import Column, String, DateTime, ARRAY
+from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
 from datetime import datetime
 from src.db.database import Base
-
+from sqlalchemy import Column, String, ARRAY, ForeignKey, DateTime
 class Note(Base):
     __tablename__ = "notes"
 
     id = Column(String, primary_key=True, default=lambda: str(uuid4()))
-    user_id = Column(String, nullable=False, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     title = Column(String)
     content = Column(String, nullable=False)
     content_json = Column(JSONB, nullable=True)  # ✅ Thêm trường mới
